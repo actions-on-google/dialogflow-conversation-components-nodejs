fc5fa90499e41017c317e8678294dae05868789f
Matt Carroll <matthewcarroll@google.com>
2017-05-16 21:57:18 -0700

    Initial checkin of conversation component sample
    Change-Id: Ia10d3d982f6d2d780949e21d9a05fa5eac26e877

---
+++ README.md
@@ -0,0 +1,49 @@
+# Actions on Google Conversation Component Sample
+
+A simple sample showing the visual conversation components available with Actions on Google.
+
+## Setup Instructions
+
+### Steps for Actions SDK
+1. Use the [Actions on Google Console](https://console.actions.google.com) to add a new project with a name of your choosing.
+1. Choose "Actions SDK"
+1. Deploy this the `actionsdk.js` app to your preferred hosting environment
+(we recommend [Google Cloud Functions](https://cloud.google.com/functions/docs/tutorials/http)).
+1. Update the action package, action.json, with your endpoint URL.
+1. Use the gActions CLI to complete the command shown in the Actions console.
+1. Click "OK" in the Actions console.
+1. Open the Simulator in the Actions console.
+1. Type "Talk to my test app" in the simulator, or say "OK Google, talk to my test app" to any Actions on Google enabled device signed into your developer account.
+
+### Steps for API.AI
+1. Use the [Actions on Google Console](https://console.actions.google.com) to add a new project with a name of your choosing.
+1. Click "Use API.AI" and then "Create Actions on API.AI".
+1. Click "Save" to save the project.
+1. Click on the gear icon to see the project settings.
+1. Select "Export and Import".
+1. Select "Restore from zip". Follow the directions to restore from the InteractiveFiction.zip in this repo.
+1. Deploy the app.js app to your preferred hosting environment (we recommend Google App Engine).
+1. In the Fulfillment page of the API.AI console, enable Webhook, set the URL to the hosting URL, then save.
+1. Open API.AI's Integrations page, open the Settings menu for Actions on Google, then click Test.
+1. Click View to open the Actions on Google simulator.
+1. Type "Talk to my test app" in the simulator, or say "OK Google, talk to my test app" to any Actions on Google enabled device signed into your developer account.
+
+
+For more detailed information on deployment, see the [documentation](https://developers.google.com/actions/samples/).
+
+## References and How to report bugs
+* Actions on Google documentation: [https://developers.google.com/actions/](https://developers.google.com/actions/).
+* If you find any issues, please open a bug here on GitHub.
+* Questions are answered on [StackOverflow](https://stackoverflow.com/questions/tagged/actions-on-google).
+
+## How to make contributions?
+Please read and follow the steps in the CONTRIBUTING.md.
+
+## License
+See LICENSE.md.
+
+## Terms
+Your use of this sample is subject to, and by using or downloading the sample files you agree to comply with, the [Google APIs Terms of Service](https://developers.google.com/terms/).
+
+## Google+
+Actions on Google Developers Community on Google+ [https://g.co/actionsdev](https://g.co/actionsdev).
---
+++ actions-sdk/index.js
@@ -0,0 +1,236 @@
+// Copyright 2017, Google, Inc.
+// Licensed under the Apache License, Version 2.0 (the 'License');
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+process.env.DEBUG = 'actions-on-google:*';
+let ActionsSdkApp = require('actions-on-google').ActionsSdkApp;
+
+// Constants for list and carousel selection
+const SELECTION_KEY_ONE = 'title';
+const SELECTION_KEY_GOOGLE_HOME = 'googleHome';
+const SELECTION_KEY_GOOGLE_PIXEL = 'googlePixel';
+const SELECTION_KEY_GOOGLE_ALLO = 'googleAllo';
+
+// Constant for image URLs
+const IMG_URL_AOG = 'https://developers.google.com/actions/images/badges' +
+  '/XPM_BADGING_GoogleAssistant_VER.png';
+const IMG_URL_GOOGLE_HOME = 'https://lh3.googleusercontent.com' +
+  '/Nu3a6F80WfixUqf_ec_vgXy_c0-0r4VLJRXjVFF_X_CIilEu8B9fT35qyTEj_PEsKw';
+const IMG_URL_GOOGLE_PIXEL = 'https://storage.googleapis.com/madebygoog/v1' +
+  '/Pixel/Pixel_ColorPicker/Pixel_Device_Angled_Black-720w.png';
+const IMG_URL_GOOGLE_ALLO = 'https://allo.google.com/images/allo-logo.png';
+
+exports.conversationComponent = (req, res) => {
+  const app = new ActionsSdkApp({request: req, response: res});
+  console.log('Request headers: ' + JSON.stringify(req.headers));
+  console.log('Request body: ' + JSON.stringify(req.body));
+
+  // Welcome
+  function welcome (app) {
+    app.ask(app.buildRichResponse()
+      .addSimpleResponse({speech: 'Hi there!', displayText: 'Hello there!'})
+      .addSimpleResponse({
+        speech: 'I can show you basic cards, lists and carousels as well as ' +
+          'suggestions on your phone',
+        displayText: 'I can show you basic cards, lists and carousels as ' +
+          'well as suggestions'})
+      .addSuggestions(
+        ['Basic Card', 'List', 'Carousel', 'Suggestions']));
+  }
+
+  function normalAsk (app) {
+    app.ask('Ask me to show you a list, carousel, or basic card');
+  }
+
+   // Suggestions
+  function suggestions (app) {
+    app.ask(app
+      .buildRichResponse()
+      .addSimpleResponse('This is a simple response for suggestions')
+      .addSuggestions('Suggestion Chips')
+      .addSuggestions(['Basic Card', 'List', 'Carousel'])
+      .addSuggestionLink('Suggestion Link', 'https://assistant.google.com/'));
+  }
+
+  // Basic card
+  function basicCard (app) {
+    app.ask(app.buildRichResponse()
+      .addSimpleResponse('This is the first simple response for a basic card')
+      .addSuggestions(
+        ['Basic Card', 'List', 'Carousel', 'Suggestions'])
+        // Create a basic card and add it to the rich response
+        .addBasicCard(app.buildBasicCard(`This is a basic card.  Text in a
+        basic card can include "quotes" and most other unicode characters 
+        including emoji 📱.  Basic cards also support some markdown 
+        formatting like *emphasis* or _italics_, **strong** or __bold__, 
+        and ***bold itallic*** or ___strong emphasis___ as well as other things
+        like line  \nbreaks`) // Note the two spaces before '\n' required for a
+                              // line break to be rendered in the card
+          .setSubtitle('This is a subtitle')
+          .setTitle('Title: this is a title')
+          .addButton('This is a button', 'https://assistant.google.com/')
+          .setImage(IMG_URL_AOG, 'Image alternate text')
+        )
+      .addSimpleResponse({ speech: 'This is the second simple response ',
+        displayText: 'This is the 2nd simple response' })
+    );
+  }
+
+  // List
+  function list (app) {
+    app.askWithList(app.buildRichResponse()
+      .addSimpleResponse('This is a simple response for a list')
+      .addSuggestions(
+        ['Basic Card', 'List', 'Carousel', 'Suggestions']),
+      // Build a list
+      app.buildList('List Title')
+        // Add the first item to the list
+        .addItems(app.buildOptionItem(SELECTION_KEY_ONE,
+          ['synonym of title 1', 'synonym of title 2', 'synonym of title 3'])
+          .setTitle('Title of First List Item')
+          .setDescription('This is a description of a list item')
+          .setImage(IMG_URL_AOG, 'Image alternate text'))
+        // Add the second item to the list
+        .addItems(app.buildOptionItem(SELECTION_KEY_GOOGLE_HOME,
+          ['Google Home Assistant', 'Assistant on the Google Home'])
+          .setTitle('Google Home')
+          .setDescription(`Google Home is a voice-activated speaker powered by
+            the Google Assistant.`)
+          .setImage(IMG_URL_GOOGLE_HOME, 'Google Home')
+        )
+        // Add third item to the list
+        .addItems(app.buildOptionItem(SELECTION_KEY_GOOGLE_PIXEL,
+          ['Google Pixel XL', 'Pixel', 'Pixel XL'])
+          .setTitle('Google Pixel')
+          .setDescription('Pixel. Phone by Google.')
+          .setImage(IMG_URL_GOOGLE_PIXEL, 'Google Pixel')
+        )
+        // Add last item of the list
+        .addItems(app.buildOptionItem(SELECTION_KEY_GOOGLE_ALLO, [])
+          .setTitle('Google Allo')
+          .setDescription('Introducing Google Allo, a smart messaging app' +
+            'that helps you say more and do more.')
+          .setImage(IMG_URL_GOOGLE_ALLO, 'Google Allo Logo')
+          .addSynonyms('Allo')
+        )
+    );
+  }
+
+  // Carousel
+  function carousel (app) {
+    app.askWithCarousel(app.buildRichResponse()
+      .addSimpleResponse('This is a simple response for a carousel')
+      .addSuggestions(
+        ['Basic Card', 'List', 'Carousel', 'Suggestions']),
+      app.buildCarousel()
+        // Add the first item to the carousel
+        .addItems(app.buildOptionItem(SELECTION_KEY_ONE,
+          ['synonym of title 1', 'synonym of title 2', 'synonym of title 3'])
+          .setTitle('Title of First List Item')
+          .setDescription('This is a description of a carousel item')
+          .setImage(IMG_URL_AOG, 'Image alternate text'))
+        // Add the second item to the carousel
+        .addItems(app.buildOptionItem(SELECTION_KEY_GOOGLE_HOME,
+          ['Google Home Assistant', 'Assistant on the Google Home'])
+          .setTitle('Google Home')
+          .setDescription(`Google Home is a voice-activated speaker powered by
+            the Google Assistant.`)
+          .setImage(IMG_URL_GOOGLE_HOME, 'Google Home')
+        )
+        // Add third item to the carousel
+        .addItems(app.buildOptionItem(SELECTION_KEY_GOOGLE_PIXEL,
+          ['Google Pixel XL', 'Pixel', 'Pixel XL'])
+          .setTitle('Google Pixel')
+          .setDescription('Pixel. Phone by Google.')
+          .setImage(IMG_URL_GOOGLE_PIXEL, 'Google Pixel')
+        )
+        // Add last item of the carousel
+        .addItems(app.buildOptionItem(SELECTION_KEY_GOOGLE_ALLO, [])
+          .setTitle('Google Allo')
+          .setDescription('Introducing Google Allo, a smart messaging app' +
+            'that helps you say more and do more.')
+          .setImage(IMG_URL_GOOGLE_ALLO, 'Google Allo Logo')
+          .addSynonyms('Allo')
+        )
+    );
+  }
+
+  // React to list or carousel selection
+  function itemSelected (app) {
+    const param = app.getSelectedOption();
+    console.log('USER SELECTED: ' + param);
+    if (!param) {
+      app.ask('You did not select any item from the list or carousel');
+    } else if (param === SELECTION_KEY_ONE) {
+      app.ask('You selected the first item in the list or carousel');
+    } else if (param === SELECTION_KEY_GOOGLE_HOME) {
+      app.ask('You selected the Google Home!');
+    } else if (param === SELECTION_KEY_GOOGLE_PIXEL) {
+      app.ask('You selected the Google Pixel!');
+    } else if (param === SELECTION_KEY_GOOGLE_ALLO) {
+      app.ask('You selected Google Allo!');
+    } else {
+      app.ask('You selected an unknown item from the list or carousel');
+    }
+  }
+
+  // Leave conversation with card
+  function byeCard (app) {
+    app.tell(app.buildRichResponse()
+      .addSimpleResponse('Goodbye, World!')
+      .addBasicCard(app.buildBasicCard('This is a goodbye card.')));
+  }
+
+  // Leave conversation with SimpleResponse
+  function byeResponse (app) {
+    app.tell({speech: 'Okay see you later',
+      displayText: 'OK see you later!'});
+  }
+
+  // Leave conversation
+  function normalBye (app) {
+    app.tell('Okay see you later!');
+  }
+
+  function actionsText (app) {
+    let rawInput = app.getRawInput();
+    console.log('USER SAID ' + rawInput);
+    if (rawInput === 'Basic Card' || rawInput === 'basic card') {
+      basicCard(app);
+    } else if (rawInput === 'List' || rawInput === 'list') {
+      list(app);
+    } else if (rawInput === 'Carousel' || rawInput === 'carousel') {
+      carousel(app);
+    } else if (rawInput === 'normal ask') {
+      normalAsk(app);
+    } else if (rawInput === 'normal bye') {
+      normalBye(app);
+    } else if (rawInput === 'bye card') {
+      byeCard(app);
+    } else if (rawInput === 'bye response') {
+      byeResponse(app);
+    } else if (rawInput === 'Suggestions' || rawInput === 'Suggestion Chips' ||
+               rawInput === 'suggestions' || rawInput === 'suggestions chips') {
+      suggestions(app);
+    } else {
+      normalAsk(app);
+    }
+  }
+
+  const actionMap = new Map();
+  actionMap.set(app.StandardIntents.MAIN, welcome);
+  actionMap.set(app.StandardIntents.TEXT, actionsText);
+  actionMap.set(app.StandardIntents.OPTION, itemSelected);
+  app.handleRequest(actionMap);
+};
